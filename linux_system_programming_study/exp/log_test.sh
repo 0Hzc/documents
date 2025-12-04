da3d28f32060123a20bef818c58dd92e429e3fb3
0hzc <1299787619@qq.com>
2025-12-04 23:21:07 +0800

    学习2

---
+++ linux_system_programming_study/exp/log_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+LOG_FILE="build.log"
+if [ -f "${LOG_FILE}" ];then
+    rm -rf ${LOG_FILE}
+else
+    echo "=== 开始构建 LFS 环境 ===">${LOG_FILE}
+    echo "正在下载源码包...">>${LOG_FILE}
+    ls /bu_cun_zai_de_wen_jian >> ${LOG_FILE} 2>&1
+    echo "脚本运行结束，以下是日志内容："
+fi
